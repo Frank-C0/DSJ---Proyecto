3bec446691c835d56d6d4cd8938d427002ad1b5d
ant25042002 <Aacot@unsa.edu.pe>
2024-11-04 01:56:36 -0500

    agregando waypoints

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class EnemyFollow : MonoBehaviour
 {
     public float walkSpeed = 2f;
+    public Transform[] waypoints;
+    private int currentWaypointIndex = 0;
     public float runSpeed = 5f;
     public float detectionRange = 60f; // Distancia a la que el rayo detectará al jugador
-    public LayerMask detectionLayer;    // Capa para raycast, asignar al jugador y evitar obstáculos
     public float stopDistance = 0.5f;
     public Transform target;
     public NavigationEnemy navigationEnemy;
@@ -16,10 +17,14 @@ public class EnemyFollow : MonoBehaviour
     private Animator animator;
     private Rigidbody rb;
     private bool isFollowing = false;
+    private float factor = 1.0f;
+
+    private float minDistanceToTarget = 2.5f;
     private Vector3 wanderingDirection;
 
     // text mesh pro for game over
     public TMPro.TextMeshProUGUI gameOverText;
+    public UnityEngine.AI.NavMeshAgent agent;
 
     void Start()
     {
@@ -30,6 +35,14 @@ public class EnemyFollow : MonoBehaviour
         // Generar una dirección inicial para deambular
         wanderingDirection = Random.insideUnitSphere;
         wanderingDirection.y = 0; // Para que solo deambule en el plano horizontal
+        navigationEnemy.SetTarget(player);
+
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            Vector3 position = waypoints[i].position;
+            position.y = transform.position.y;    // Asigna el valor de 'y' fijo
+            waypoints[i].position = position; // Actualiza la posición del waypoint
+        }
     }
 
     void Update()
@@ -43,7 +56,7 @@ public class EnemyFollow : MonoBehaviour
         }
         else
         {
-            Wandering();
+            MoveTowardsWaypoint();
         }
     }
 
@@ -61,9 +74,10 @@ public class EnemyFollow : MonoBehaviour
             // Si el objeto detectado es el jugador
             if (hit.collider.CompareTag("Player"))
             {
-                Debug.Log("Objeto detectado: " + hit.collider.name);
+                //Debug.Log("Objeto detectado: " + hit.collider.name);
                 isFollowing = true;
-                navigationEnemy.isActive= false;
+                //navigationEnemy.isActive= false;
+                navigationEnemy.SetBoolIsActive(true);
                 animator.SetBool("isWalking", false);
                 animator.SetBool("isRunning", true);
             }
@@ -71,8 +85,18 @@ public class EnemyFollow : MonoBehaviour
         else
         {
             // Si no detecta al jugador, dejar de seguirlo
-            isFollowing = false;
-            navigationEnemy.isActive= true;
+            // Activar la animación de caminar mientras deambula
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isRunning", false);
+            navigationEnemy.SetBoolIsActive(false);
+            float distanceToPath = Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position);
+            if (distanceToPath > 0.1f){
+                agent.enabled= true;
+                agent.SetDestination(waypoints[currentWaypointIndex].position);
+            } else{
+                agent.enabled= false;
+                isFollowing = false;
+            }
         }
     }
 
@@ -83,8 +107,16 @@ public class EnemyFollow : MonoBehaviour
 
         if (distanceToPlayer > stopDistance)
         {
+            float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+            if ((distanceToTarget < minDistanceToTarget)){
+                factor= 0.8f;
+            }else{
+                factor= 1.0f;
+            }
+            
             Vector3 directionToPlayer = (target.position - transform.position).normalized;
-            MoveTowards(directionToPlayer, 2*runSpeed);
+            MoveTowards(directionToPlayer, 2*runSpeed*factor);
         }
         else
         {
@@ -106,16 +138,48 @@ public class EnemyFollow : MonoBehaviour
         MoveTowards(wanderingDirection, walkSpeed);
     }
 
+    void MoveTowardsWaypoint()
+    {
+        if (waypoints.Length == 0) return; // Si no hay waypoints, salir
+
+        // Moverse hacia el waypoint actual
+        Transform target = waypoints[currentWaypointIndex];
+        Vector3 direction = (target.position - transform.position).normalized;
+        MoveTowards(direction, walkSpeed);
+        /*Vector3 direction = (target.position - transform.position).normalized;
+        transform.position += direction * walkSpeed * Time.deltaTime;*/
+
+        // Comprobar si ha llegado al waypoint
+        if (Vector3.Distance(transform.position, target.position) < 0.8f)
+        {
+            // Decidir si continuar al siguiente waypoint o volver al anterior
+            DecideNextWaypoint();
+        }
+    }
+
+    void DecideNextWaypoint()
+    {
+        float randomValue = Random.value; // Genera un valor aleatorio entre 0 y 1
+
+        if (randomValue < 0.7f) // 70% de probabilidad de continuar
+        {
+            // Mover al siguiente waypoint
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length; // Cíclico
+        }
+        else // 30% de probabilidad de volver al anterior
+        {
+            // Mover al waypoint anterior
+            currentWaypointIndex = (currentWaypointIndex - 1 + waypoints.Length) % waypoints.Length; // Cíclico
+        }
+        Debug.Log(currentWaypointIndex);
+    }
+
     // Método para mover el objeto en una dirección con cierta velocidad
     void MoveTowards(Vector3 direction, float speed)
     {
         Vector3 newPosition = rb.position + direction * speed * Time.deltaTime;
         rb.MovePosition(newPosition);
-        this.transform.LookAt(target.position);
-
-        // Rotar para mirar en la dirección de movimiento
-        //Quaternion lookRotation = Quaternion.LookRotation(direction);
-        //rb.MoveRotation(Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f));
+        this.transform.LookAt(transform.position + direction);
     }
 
     // Cambiar de dirección si colisiona con una pared u obstáculo
@@ -124,8 +188,17 @@ public class EnemyFollow : MonoBehaviour
         if (!collision.gameObject.CompareTag("Player"))
         {
             // Generar una nueva dirección aleatoria al chocar con algo
-            wanderingDirection = Random.insideUnitSphere;
+            //wanderingDirection = Random.insideUnitSphere;
+            //wanderingDirection.y = 0;
+            // Obtener la normal de la colisión (dirección perpendicular a la pared)
+            Vector3 normalColision = collision.contacts[0].normal;
+
+            // Generar una dirección opuesta a la pared usando la normal
+            wanderingDirection = Vector3.Reflect(transform.forward, normalColision) + Random.insideUnitSphere;
+            
+            // Asegurarse de que la dirección esté en el plano horizontal
             wanderingDirection.y = 0;
+
         }
         else
         {
